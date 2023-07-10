ca2a0e1c06bca58de19471151c3e4e2e3ac7ec72
PatilKartik1 <patilkartik94@gmail.com>
2023-07-10 13:03:07 +0530

    added new program

--- leet1281.cpp
+++ leet1281.cpp
@@ -1,7 +1,26 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+
+int digitProductSumDifference(int n)
+{
+    int product = 1;
+    int sum = 0;
+
+    while (n > 0)
+    {
+        int digit = n % 10;
+        product *= digit;
+        sum += digit;
+        n /= 10;
+    }
+
+    return product - sum;
+}
+
 int main()
 {
+    int n = 234;
+    int difference = digitProductSumDifference(n);
+    std::cout << "Difference: " << difference << std::endl;
 
-
+    return 0;
 }
