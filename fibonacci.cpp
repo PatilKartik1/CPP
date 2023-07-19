5e63fb6c150bcdb21372f2f59c867564fb3d4102
PatilKartik1 <patilkartik94@gmail.com>
2023-07-19 22:32:38 +0530

    made changes

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 int main()
 {
-    int n = 10;
+    int n;
+    cout << "Enter n(n>2)";
+    cin >> n;
     int a = 0, b = 1;
+    cout << a << "\t" << b << "\t";
     for (int i = 1; i < n; i++)
     {
         int nextf = a + b;
-        cout << nextf << endl;
+        cout << nextf << "\t";
 
         a = b;
         b = nextf;
