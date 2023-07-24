b97bf51e05c13206f59a2af20cf4f149efa1b636
PatilKartik1 <patilkartik94@gmail.com>
2023-07-24 23:06:26 +0530

    added calculator and switchcase program

---
+++ calculator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    char choice;
+
+    cout << "Enter choice(+,-,*,/,%)";
+    cin >> choice;
+
+    cout << "Enter value of a and b:";
+    cin >> a >> b;
+    while (1)
+    {
+        switch (choice)
+        {
+        case '+':
+            cout << a + b << endl;
+            break;
+
+        case '-':
+            cout << a - b << endl;
+            break;
+
+        case '*':
+            cout << a * b << endl;
+            break;
+
+        case '/':
+            cout << a / b << endl;
+            break;
+
+        case '%':
+            cout << a % b << endl;
+            break;
+
+        default:
+            cout << "Please enter a valid operation" << endl;
+            break;
+            exit(0);
+        }
+    }
+}
--- switchcase.cpp
+++ switchcase.cpp
@@ -2,19 +2,35 @@
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
+    int amount;
+    int Rs100, Rs50, Rs20, Rs1;
+    cin >> amount;
 
-    switch (n % 2)
+    switch (1)
     {
-    case 0:
-        cout << "No is Even" << endl;
-        break;
     case 1:
-        cout << "No is Odd" << endl;
+        Rs100 = amount / 100;
+        amount = amount % 100;
+        cout << "100 Rs note are " << Rs100 << endl;
+
+    case 2:
+        Rs50 = amount / 50;
+        amount = amount % 50;
+        cout << "50 Rs note are " << Rs50 << endl;
+
+    case 3:
+        Rs20 = amount / 20;
+        amount = amount % 20;
+        cout << "20 Rs note are " << Rs20 << endl;
+
+    case 4:
+        Rs1 = amount / 1;
+        amount = amount % 1;
+        cout << "1 Rs note are " << Rs1 << endl;
+        break;
 
     default:
-        cout << "Invalid no" << endl;
+        cout << "Not valid " << endl;
         break;
     }
 }
