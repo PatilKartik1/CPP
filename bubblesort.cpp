839c33ed8129ca7eac0f9ee8bd873d57bcfa22db
PatilKartik1 <patilkartik94@gmail.com>
2023-08-29 21:25:52 +0530

    made changes in bubblesort

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -11,15 +11,13 @@ int main()
         cin >> arr[i];
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        for (int j = 0; j < (n - i - 1); j++)
+        for (int j = 0; j < (n - i); j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                 temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
