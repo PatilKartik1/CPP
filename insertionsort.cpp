eb113f12c14d1bb1a9997f19bf83069e07c3c519
PatilKartik1 <patilkartik94@gmail.com>
2023-08-30 20:05:47 +0530

    added insertionsort program.

---
+++ insertionsort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+}
